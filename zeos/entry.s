bed676f752e781e59c9a0876f1310fe1f0fee768
fredy <fredy.gustavo.gallegos@estudiantat.upc.edu>
2021-03-10 15:45:57 +0100

    function write problem solved

--- zeos/entry.s
+++ zeos/entry.s
@@ -25,13 +25,13 @@
 .globl system_call_handler; .type system_call_handler, @function; .align 0; system_call_handler:
  pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es;
  cmpl $0, %EAX
- jl sysenter_err
+ jl err
  cmpl $MAX_SYSCALL, %EAX
  jg err
  call *sys_call_table(, %EAX, 0x04)
  jmp fin
 err:
- movl $-ENOSYS, %EAX
+ movl $38, %EAX
 fin:
  movl %EAX, 0x18(%ESP)
  popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs;
--- zeos/wrappers.s
+++ zeos/wrappers.s
@@ -1,19 +1,27 @@
-#include <asm.h>
+# 1 "wrappers.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "wrappers.S"
+# 1 "include/asm.h" 1
+# 2 "wrappers.S" 2
 
-ENTRY(write)
+.globl write; .type write, @function; .align 0; write:
     pushl %ebp
     mov %esp, %ebp
-    pushl %ecx 
-    pushl %edx  
+    pushl %ecx
+    pushl %edx
     mov 8(%ebp), %ebx # fd
     mov 12(%ebp), %ecx # buffer
     mov 0X10(%ebp), %edx # size
 
     mov $0x04, %eax # access the 4th position in the sys_call_table
 
-    pushl $write_return  
-    pushl %ebp 
-    mov %esp, %ebp 
+    pushl $write_return
+    pushl %ebp
+    mov %esp, %ebp
     sysenter
 
 write_return:
